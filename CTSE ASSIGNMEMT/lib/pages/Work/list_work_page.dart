75d6c347c96370f67e5c4dcf966abd33f330cdc4
RDharsan <dharsan.3399@gmail.com>
2023-03-23 17:27:01 +0530

    list work added

---
+++ CTSE ASSIGNMEMT/lib/pages/Work/list_work_page.dart
@@ -0,0 +1,155 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_fbfirestore_crud/pages/Work/update_work_page.dart';
+
+class ListWork extends StatefulWidget {
+  ListWork({Key? key}) : super(key: key);
+
+  @override
+  _ListWorkState createState() => _ListWorkState();
+}
+
+class _ListWorkState extends State<ListWork> {
+  final Stream<QuerySnapshot> worksStream =
+      FirebaseFirestore.instance.collection('work').snapshots();
+
+  // For Deleting User
+  CollectionReference works = FirebaseFirestore.instance.collection('work');
+  Future<void> deleteWork(id) {
+    // print("User Deleted $id");
+    return works
+        .doc(id)
+        .delete()
+        .then((value) => print('Work Notes Deleted'))
+        .catchError((error) => print('Failed to Delete Notes: $error'));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<QuerySnapshot>(
+        stream: worksStream,
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (snapshot.hasError) {
+            print('Something went Wrong');
+          }
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+
+          final List storedocs = [];
+          snapshot.data!.docs.map((DocumentSnapshot document) {
+            Map a = document.data() as Map<String, dynamic>;
+            storedocs.add(a);
+            a['id'] = document.id;
+          }).toList();
+
+          return Container(
+            margin: EdgeInsets.symmetric(horizontal: 10.0, vertical: 20.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Center(
+                  child: Text(
+                    'Work Notes List',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 24.0,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: 'Times New Roman',
+                    ),
+                  ),
+                ),
+                SizedBox(height: 20.0),
+                Expanded(
+                  child: ListView.builder(
+                    itemCount: storedocs.length,
+                    itemBuilder: (context, index) {
+                      return Card(
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    'Title: ${storedocs[index]['title']}',
+                                    style: TextStyle(
+                                      fontSize: 18.0,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                  SizedBox(height: 5.0),
+                                  Text(
+                                    'Description: ${storedocs[index]['description']}',
+                                    style: TextStyle(
+                                      fontSize: 16.0,
+                                    ),
+                                  ),
+                                  SizedBox(height: 5.0),
+                                  Text(
+                                    'Progress: ${storedocs[index]['progress']}',
+                                    style: TextStyle(
+                                      fontSize: 16.0,
+                                    ),
+                                  ),
+                                  SizedBox(height: 5.0),
+                                  Text(
+                                    'Deadline:${storedocs[index]['deadline']}',
+                                    style: TextStyle(
+                                      fontSize: 16.0,
+                                    ),
+                                  ),
+                                  SizedBox(height: 5.0),
+                                  Text(
+                                    'Date: ${storedocs[index]['date']}',
+                                    style: TextStyle(
+                                      fontSize: 16.0,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              Row(
+                                children: [
+                                  IconButton(
+                                    onPressed: () => {
+                                      Navigator.push(
+                                        context,
+                                        MaterialPageRoute(
+                                          builder: (context) => UpdateWork(
+                                              id: storedocs[index]['id']),
+                                        ),
+                                      )
+                                    },
+                                    icon: Icon(
+                                      Icons.edit,
+                                      color: Colors.orange,
+                                    ),
+                                  ),
+                                  IconButton(
+                                    onPressed: () =>
+                                        {deleteWork(storedocs[index]['id'])},
+                                    icon: Icon(
+                                      Icons.delete,
+                                      color: Colors.red,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+                      );
+                    },
+                  ),
+                ),
+              ],
+            ),
+          );
+        });
+  }
+}
