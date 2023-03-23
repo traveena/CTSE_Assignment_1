c50469074ae7f69083251a0537d1c6822b09fc34
Traveena <traveenachandrasegar@gmail.com>
2023-03-23 16:24:39 +0530

    Study_list

---
+++ CTSE ASSIGNMEMT/lib/pages/Study/list_study_page.dart
@@ -0,0 +1,155 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_fbfirestore_crud/pages/Study/update_study_page.dart';
+
+class ListStudy extends StatefulWidget {
+  ListStudy({Key? key}) : super(key: key);
+
+  @override
+  _ListStudyState createState() => _ListStudyState();
+}
+
+class _ListStudyState extends State<ListStudy> {
+  final Stream<QuerySnapshot> studiesStream =
+      FirebaseFirestore.instance.collection('study').snapshots();
+
+  // For Deleting User
+  CollectionReference studies = FirebaseFirestore.instance.collection('study');
+  Future<void> deleteStudy(id) {
+    // print("User Deleted $id");
+    return studies
+        .doc(id)
+        .delete()
+        .then((value) => print('Study Notes Deleted'))
+        .catchError((error) => print('Failed to Delete Notes: $error'));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<QuerySnapshot>(
+        stream: studiesStream,
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (snapshot.hasError) {
+            print('Something went Wrong');
+          }
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+
+          final List storedocs = [];
+          snapshot.data!.docs.map((DocumentSnapshot document) {
+            Map a = document.data() as Map<String, dynamic>;
+            storedocs.add(a);
+            a['id'] = document.id;
+          }).toList();
+
+          return Container(
+            margin: EdgeInsets.symmetric(horizontal: 10.0, vertical: 20.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Center(
+                  child: Text(
+                    'Study Notes List',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 24.0,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: 'Times New Roman',
+                    ),
+                  ),
+                ),
+                SizedBox(height: 20.0),
+                Expanded(
+                  child: ListView.builder(
+                    itemCount: storedocs.length,
+                    itemBuilder: (context, index) {
+                      return Card(
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    'Subject Name: ${storedocs[index]['subjectName']}',
+                                    style: TextStyle(
+                                      fontSize: 18.0,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                  SizedBox(height: 5.0),
+                                  Text(
+                                    'Description: ${storedocs[index]['description']}',
+                                    style: TextStyle(
+                                      fontSize: 16.0,
+                                    ),
+                                  ),
+                                  SizedBox(height: 5.0),
+                                  Text(
+                                    'Completeion Status: ${storedocs[index]['completionStatus']}',
+                                    style: TextStyle(
+                                      fontSize: 16.0,
+                                    ),
+                                  ),
+                                  SizedBox(height: 5.0),
+                                  Text(
+                                    'Due Date:${storedocs[index]['dueDate']}',
+                                    style: TextStyle(
+                                      fontSize: 16.0,
+                                    ),
+                                  ),
+                                  SizedBox(height: 5.0),
+                                  Text(
+                                    'Date: ${storedocs[index]['date']}',
+                                    style: TextStyle(
+                                      fontSize: 16.0,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              Row(
+                                children: [
+                                  IconButton(
+                                    onPressed: () => {
+                                      Navigator.push(
+                                        context,
+                                        MaterialPageRoute(
+                                          builder: (context) => UpdateStudy(
+                                              id: storedocs[index]['id']),
+                                        ),
+                                      )
+                                    },
+                                    icon: Icon(
+                                      Icons.edit,
+                                      color: Colors.orange,
+                                    ),
+                                  ),
+                                  IconButton(
+                                    onPressed: () =>
+                                        {deleteStudy(storedocs[index]['id'])},
+                                    icon: Icon(
+                                      Icons.delete,
+                                      color: Colors.red,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+                      );
+                    },
+                  ),
+                ),
+              ],
+            ),
+          );
+        });
+  }
+}
