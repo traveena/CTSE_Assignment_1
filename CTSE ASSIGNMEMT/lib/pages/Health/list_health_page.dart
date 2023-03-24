d919c0d2f7eff07df77ed1cffaf1e5688e402e39
Krishanthini <it19980928@my.sliit.lk>
2023-03-24 14:41:24 +0530

    health list added

---
+++ CTSE ASSIGNMEMT/lib/pages/Health/list_health_page.dart
@@ -0,0 +1,162 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_fbfirestore_crud/pages/Health/update_health_page.dart';
+
+class ListHealth extends StatefulWidget {
+  ListHealth({Key? key}) : super(key: key);
+
+  @override
+  _ListHealthState createState() => _ListHealthState();
+}
+
+class _ListHealthState extends State<ListHealth> {
+  final Stream<QuerySnapshot> healthsStream =
+      FirebaseFirestore.instance.collection('health').snapshots();
+
+  // For Deleting User
+  CollectionReference healths = FirebaseFirestore.instance.collection('health');
+  Future<void> deleteHealth(id) {
+    // print("User Deleted $id");
+    return healths
+        .doc(id)
+        .delete()
+        .then((value) => print('Health Notes Deleted'))
+        .catchError((error) => print('Failed to Delete Notes: $error'));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<QuerySnapshot>(
+        stream: healthsStream,
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (snapshot.hasError) {
+            print('Something went Wrong');
+          }
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+
+          final List storedocs = [];
+          snapshot.data!.docs.map((DocumentSnapshot document) {
+            Map a = document.data() as Map<String, dynamic>;
+            storedocs.add(a);
+            a['id'] = document.id;
+          }).toList();
+
+          return Container(
+            margin: EdgeInsets.symmetric(horizontal: 10.0, vertical: 20.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Center(
+                  child: Text(
+                    'Health Notes List',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 24.0,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: 'Times New Roman',
+                    ),
+                  ),
+                ),
+                SizedBox(height: 20.0),
+                Expanded(
+                  child: ListView.builder(
+                    itemCount: storedocs.length,
+                    itemBuilder: (context, index) {
+                      return Card(
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    'Day: ${storedocs[index]['day']}',
+                                    style: TextStyle(
+                                      fontSize: 18.0,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                  SizedBox(height: 5.0),
+                                  Text(
+                                    'Date: ${storedocs[index]['date']}',
+                                    style: TextStyle(
+                                      fontSize: 16.0,
+                                    ),
+                                  ),
+                                  SizedBox(height: 5.0),
+                                  Text(
+                                    'Water Amount(L): ${storedocs[index]['wateramount']}',
+                                    style: TextStyle(
+                                      fontSize: 16.0,
+                                    ),
+                                  ),
+                                  SizedBox(height: 5.0),
+                                  Text(
+                                    'Calories:${storedocs[index]['calories']}',
+                                    style: TextStyle(
+                                      fontSize: 16.0,
+                                    ),
+                                  ),
+                                  SizedBox(height: 5.0),
+                                  Text(
+                                    'Exercise Hours: ${storedocs[index]['exerciseHours']}',
+                                    style: TextStyle(
+                                      fontSize: 16.0,
+                                    ),
+                                  ),
+                                  SizedBox(height: 5.0),
+                                  Text(
+                                    'Burn Calories:${storedocs[index]['burnCalories']}',
+                                    style: TextStyle(
+                                      fontSize: 16.0,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              Row(
+                                children: [
+                                  IconButton(
+                                    onPressed: () => {
+                                      Navigator.push(
+                                        context,
+                                        MaterialPageRoute(
+                                          builder: (context) => UpdateHealth(
+                                              id: storedocs[index]['id']),
+                                        ),
+                                      )
+                                    },
+                                    icon: Icon(
+                                      Icons.edit,
+                                      color: Colors.orange,
+                                    ),
+                                  ),
+                                  IconButton(
+                                    onPressed: () =>
+                                        {deleteHealth(storedocs[index]['id'])},
+                                    icon: Icon(
+                                      Icons.delete,
+                                      color: Colors.red,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+                      );
+                    },
+                  ),
+                ),
+              ],
+            ),
+          );
+        });
+  }
+}
