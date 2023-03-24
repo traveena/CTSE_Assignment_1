2ddd7b879f812f1fd1991ba67b99380d2fc72041
RDharsan <dharsan.3399@gmail.com>
2023-03-24 13:35:16 +0530

    updated home page

---
+++ CTSE ASSIGNMEMT/lib/pages/Work/update_work_page.dart
@@ -0,0 +1,208 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+class UpdateWork extends StatefulWidget {
+  final String id;
+  UpdateWork({Key? key, required this.id}) : super(key: key);
+
+  @override
+  _UpdateWorkState createState() => _UpdateWorkState();
+}
+
+class _UpdateWorkState extends State<UpdateWork> {
+  final _formKey = GlobalKey<FormState>();
+
+  // Updaing Student
+  CollectionReference students = FirebaseFirestore.instance.collection('work');
+
+  Future<void> updateWork(id, title, description, progress, deadline, date) {
+    return students
+        .doc(id)
+        .update({
+          'title': title,
+          'description': description,
+          'progress': progress,
+          'deadline': deadline,
+          'date': date
+        })
+        .then((value) => print("Work notes Updated"))
+        .catchError((error) => print("Failed to update work notes: $error"));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Update Work Notes List"),
+      ),
+      body: Form(
+          key: _formKey,
+          // Getting Specific Data by ID
+          child: FutureBuilder<DocumentSnapshot<Map<String, dynamic>>>(
+            future: FirebaseFirestore.instance
+                .collection('work')
+                .doc(widget.id)
+                .get(),
+            builder: (_, snapshot) {
+              if (snapshot.hasError) {
+                print('Something Went Wrong');
+              }
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return Center(
+                  child: CircularProgressIndicator(),
+                );
+              }
+              var data = snapshot.data!.data();
+              var title = data!['title'];
+              var description = data['description'];
+              var progress = data['progress'];
+              var deadline = data['deadline'];
+              var date = data['date'];
+
+              return Padding(
+                padding: EdgeInsets.symmetric(vertical: 20, horizontal: 30),
+                child: ListView(
+                  children: [
+                    Container(
+                      margin: EdgeInsets.symmetric(vertical: 10.0),
+                      child: TextFormField(
+                        initialValue: title,
+                        autofocus: false,
+                        onChanged: (value) => title = value,
+                        decoration: InputDecoration(
+                          labelText: 'title: ',
+                          labelStyle: TextStyle(fontSize: 20.0),
+                          border: OutlineInputBorder(),
+                          errorStyle:
+                              TextStyle(color: Colors.redAccent, fontSize: 15),
+                        ),
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please Enter title';
+                          }
+                          return null;
+                        },
+                      ),
+                    ),
+                    Container(
+                      margin: EdgeInsets.symmetric(vertical: 10.0),
+                      child: TextFormField(
+                        initialValue: description,
+                        autofocus: false,
+                        onChanged: (value) => description = value,
+                        decoration: InputDecoration(
+                          labelText: 'description: ',
+                          labelStyle: TextStyle(fontSize: 20.0),
+                          border: OutlineInputBorder(),
+                          errorStyle:
+                              TextStyle(color: Colors.redAccent, fontSize: 15),
+                        ),
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please Enter description';
+                          }
+                          return null;
+                        },
+                      ),
+                    ),
+                    Container(
+                      margin: EdgeInsets.symmetric(vertical: 10.0),
+                      child: TextFormField(
+                        initialValue: progress,
+                        autofocus: false,
+                        onChanged: (value) => progress = value,
+                        decoration: InputDecoration(
+                          labelText: 'progress: ',
+                          labelStyle: TextStyle(fontSize: 20.0),
+                          border: OutlineInputBorder(),
+                          errorStyle:
+                              TextStyle(color: Colors.redAccent, fontSize: 15),
+                        ),
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please Enter progress';
+                          }
+                          return null;
+                        },
+                      ),
+                    ),
+                    Container(
+                      margin: EdgeInsets.symmetric(vertical: 10.0),
+                      child: TextFormField(
+                        initialValue: deadline,
+                        autofocus: false,
+                        onChanged: (value) => deadline = value,
+                        decoration: InputDecoration(
+                          labelText: 'deadline: ',
+                          labelStyle: TextStyle(fontSize: 20.0),
+                          border: OutlineInputBorder(),
+                          errorStyle:
+                              TextStyle(color: Colors.redAccent, fontSize: 15),
+                        ),
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please Enter deadline';
+                          }
+                          return null;
+                        },
+                      ),
+                    ),
+                    Container(
+                      margin: EdgeInsets.symmetric(vertical: 10.0),
+                      child: TextFormField(
+                        initialValue: date,
+                        autofocus: false,
+                        onChanged: (value) => date = value,
+                        decoration: InputDecoration(
+                          labelText: 'date: ',
+                          labelStyle: TextStyle(fontSize: 20.0),
+                          border: OutlineInputBorder(),
+                          errorStyle:
+                              TextStyle(color: Colors.redAccent, fontSize: 15),
+                        ),
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please Enter date';
+                          }
+                          return null;
+                        },
+                      ),
+                    ),
+                    Container(
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          ElevatedButton(
+                            onPressed: () {
+                              // Validate returns true if the form is valid, otherwise false.
+                              if (_formKey.currentState!.validate()) {
+                                updateWork(widget.id, title, description,
+                                    progress, deadline, date);
+                                Navigator.pop(context);
+                              }
+                            },
+                            child: Text(
+                              'Update',
+                              style: TextStyle(fontSize: 18.0),
+                            ),
+                          ),
+                          ElevatedButton(
+                            onPressed: () => {},
+                            child: Text(
+                              'Reset',
+                              style: TextStyle(fontSize: 18.0),
+                            ),
+                            style: ElevatedButton.styleFrom(
+                                primary: Colors.blueGrey),
+                          ),
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              );
+            },
+          )),
+    );
+  }
+}
